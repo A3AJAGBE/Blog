57c1560e2f6b67bacc9d8d0588d9e55de2f7307f
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-03-04 04:34:50 +0000

    Add new objects

--- blog.py
+++ blog.py
@@ -1,6 +1,9 @@
 class Blog:
-    def __init__(self, id, title, subtitle, body):
+    def __init__(self, id, title, subtitle, body, image_url, date, author):
         self.id = id
         self.title = title
         self.subtitle = subtitle
         self.body = body
+        self.image = image_url
+        self.date = date
+        self.author = author
