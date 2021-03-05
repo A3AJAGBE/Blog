16259509478c4f9359c5216547ccca4de0875628
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-03-05 09:29:30 +0000

    Add form functionality

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from datetime import datetime
 from blog import Blog
 import requests
@@ -9,7 +9,8 @@ current_year = datetime.now().year
 blogs = requests.get("https://api.npoint.io/43644ec4f0013682fc0d").json()
 blog_list = []
 for blog in blogs:
-    blog = Blog(blog["id"], blog["title"], blog["subtitle"], blog["body"], blog["image_url"], blog["date"], blog["author"])
+    blog = Blog(blog["id"], blog["title"], blog["subtitle"], blog["body"], blog["image_url"], blog["date"],
+                blog["author"])
     blog_list.append(blog)
 
 app = Flask(__name__)
@@ -25,9 +26,20 @@ def about():
     return render_template('about.html', year=current_year)
 
 
-@app.route('/contact')
+@app.route('/contact', methods=['POST', 'GET'])
 def contact():
-    return render_template('contact.html', year=current_year)
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        message = request.form['message']
+        message_info = f"The following information has been sent successfully; " \
+                       f"Name: {name}, " \
+                       f"Email: {email}, " \
+                       f"Phone Number: {phone}, " \
+                       f"Message: {message}"
+        return render_template('contact.html', year=current_year, sent=True, message_info=message_info)
+    return render_template('contact.html', year=current_year, sent=False)
 
 
 @app.route("/blog/<int:blog_id>")
