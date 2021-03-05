525867f3ee4b19642f608324a8dc1e3888771146
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-03-05 10:09:54 +0000

    Add smtlib

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+import os
+
 from flask import Flask, render_template, request
 from datetime import datetime
 from blog import Blog
 import requests
+import smtplib
+from dotenv import load_dotenv
+load_dotenv()
 
 # Get the year
 current_year = datetime.now().year
+
+# Email config
+EMAIL = os.environ.get('GMAIL')
+PASSWORD = os.environ.get('GMAIL_PASS')
+RECEIVER_EMAIL = os.environ.get('YMAIL')
 
 blogs = requests.get("https://api.npoint.io/43644ec4f0013682fc0d").json()
 blog_list = []
@@ -33,12 +43,20 @@ def contact():
         email = request.form['email']
         phone = request.form['phone']
         message = request.form['message']
-        message_info = f"The following information has been sent successfully; " \
-                       f"Name: {name}, " \
-                       f"Email: {email}, " \
-                       f"Phone Number: {phone}, " \
+
+        message_info = f"The following information was submitted: \n" \
+                       f"Name: {name} \n" \
+                       f"Email: {email} \n" \
+                       f"Phone Number: {phone} \n" \
                        f"Message: {message}"
-        return render_template('contact.html', year=current_year, sent=True, message_info=message_info)
+
+        with smtplib.SMTP("smtp.gmail.com") as conn:
+            conn.starttls()
+            conn.login(user=EMAIL, password=PASSWORD)
+            conn.sendmail(from_addr=EMAIL,
+                          to_addrs=RECEIVER_EMAIL,
+                          msg=f"Subject:New Blog Message!!!\n\n{message_info}")
+        return render_template('contact.html', year=current_year, sent=True, name=name)
     return render_template('contact.html', year=current_year, sent=False)
 
 
